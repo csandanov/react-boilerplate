d42bfff102cbf5bb57b5f8b20942b60259c5d24c
Chingis Sandanov <chingis@chingis.io>
2025-12-23 14:27:33 +0800

    Add react app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# html
+# Simple React app boilerplate
+
+Boilerplate for Wodby 2.0 static HTML app.
+
+https://github.com/wodby/service-nginx
+
+https://github.com/wodby/stack-html
+
+https://github.com/stacks/html
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+            <h1>Hello World</h1>
+            <p>This is a static React application.</p>
+        </div>
+    )
+}
+
+export default App
